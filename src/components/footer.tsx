6fb13e82c810d1dcc14a012ce78af7a210f2f7fc
Jhaymag <josephkkmmj@gmail.com>
2025-07-17 19:35:01 +0000

    minor changes to the footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -28,6 +28,7 @@ const Footer: React.FC = () => {
           <div>
             <h3 className="font-bold text-lg mb-4">Connect With Us</h3>
             <div className="flex space-x-4">
+              {/* Add facebook link and create other social links platform*/}
               <a href="#" className="hover:text-gray-300" aria-label="Facebook">
                 <svg className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
                   <path fillRule="evenodd" d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.664l-.53 2.89h-2.134v6.987C18.343 21.128 22 16.991 22 12z" clipRule="evenodd" />
